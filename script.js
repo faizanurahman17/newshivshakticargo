6a86257580ec3a6e1460be0d1bdebb4272589b5a
Faizanur Rahman <faizanurahman@Rahmans-MacBook-Air.local>
2025-01-28 18:19:00 +0530

    first page almost done

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function name(params) {
+const btns = document.querySelectorAll('.btns');
+
+btns.forEach(btn => {
     
-}
+    let onbtn = false;
+    btn.addEventListener('click',() => {
+
+        
+        onbtn = !onbtn;
+
+        if (onbtn) {
+           btn.classList.add('onclick-btns'); 
+        } else if (!onbtn) {
+            btn.classList.remove('onclick-btns');
+        }
+    });
+});
